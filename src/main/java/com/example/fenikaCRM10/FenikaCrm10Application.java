66644ee4fcb042aca667464294b2b49ec1574566
JojoPanamo <117886169+JojoPanamo@users.noreply.github.com>
2024-01-11 11:21:15 -0300

    Add files via upload

---
+++ src/main/java/com/example/fenikaCRM10/FenikaCrm10Application.java
@@ -0,0 +1,13 @@
+package com.example.fenikaCRM10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FenikaCrm10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FenikaCrm10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fenikaCRM10/FenikaCrm10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fenikaCRM10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FenikaCrm10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
